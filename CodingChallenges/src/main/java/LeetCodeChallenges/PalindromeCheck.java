d19dc9c883d7936df48b8014e168d4ccf24fa988
Raji Thomas <rajithomas16@gmail.com>
2026-04-01 14:31:49 -0400

    Leetcode 4

---
+++ CodingChallenges/src/main/java/LeetCodeChallenges/PalindromeCheck.java
@@ -0,0 +1,38 @@
+package LeetCodeChallenges;
+
+import java.util.Arrays;
+
+public class PalindromeCheck {
+    public static boolean isPalindrome(String s) {
+
+        String cleanString = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+
+        int left = 0;
+        int right = cleanString.length() - 1;
+
+        while (left < right) {
+            if (cleanString.charAt(left) != cleanString.charAt(right))
+            {
+                return false;
+            }
+
+
+            left++;
+            right--;
+
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args)
+    {
+
+        System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
+
+    }
+
+}
+
+
+
